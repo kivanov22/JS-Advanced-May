42e27585ab54952bf726f69f116ae090b31338d7
Kristian Ivanov <kriskopk@abv.bg>
2021-05-05 23:54:38 +0300

    String Length

---
+++ Syntax-Functions-and-Statements/Lab/StringLength.js
@@ -0,0 +1,13 @@
+function solve(input1,input2,input3){
+
+let firstInput=input1.length;
+let secondInput=input2.length;
+let thirdInput =input3.length;
+
+let sumLenght = firstInput + secondInput+ thirdInput;
+let averageLength = Math.floor(sumLenght/3);
+
+console.log(sumLenght);
+console.log(averageLength);
+}
+//solve('pasta', '5', '22.3');
