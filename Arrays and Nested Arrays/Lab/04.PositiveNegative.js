07789bb8529d5237f46f7ec0ca7d6a0563470c71
Kristian Ivanov <kriskopk@abv.bg>
2021-05-13 23:51:00 +0300

    Positive Negative

---
+++ Arrays and Nested Arrays/Lab/04.PositiveNegative.js
@@ -0,0 +1,16 @@
+function solve(numbers){
+
+    const result =[];
+    
+    for (const num of numbers) {
+        if (num<0) {
+        result.unshift(num);
+        }else{
+            result.push(num);
+        }
+    }
+    console.log(result.join('\n'));
+}
+
+console.log(solve([7, -2, 8, 9]));
+console.log(solve([3, -2, 0, -1]));
