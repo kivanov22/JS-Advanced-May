506d7ec5619dc1d1fc5e4da8f713b2724f7c0478
Kristian Ivanov <kriskopk@abv.bg>
2021-05-20 19:37:52 +0300

    Updated

--- Arrays and Nested Arrays/Exercise/05.ExtractIncreasingSubsequentArray.js
+++ Arrays and Nested Arrays/Exercise/05.ExtractIncreasingSubsequentArray.js
@@ -1,15 +1,22 @@
 function solve(arr) {
-  let result = [];
-  let current = 0;
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
+  // let result = [];
+  // let current = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   const element = arr[i];
 
-    if (element >= current) {
-      current = element;
-      result.push(current);
+  //   if (element >= current) {
+  //     current = element;
+  //     result.push(current);
+  //   }
+  // }
+  // return result;
+  return  arr.reduce(function(result,currentValue,index,initialArray){
+    if (currentValue>=result[result.length-1] || result.length===0) {
+     result.push(currentValue);
+     return result;
     }
-  }
-  return result;
+    return result
+  },[[]])
 }
 console.log(solve([1, 3, 8, 4, 10, 12, 3, 2, 24]));
 
