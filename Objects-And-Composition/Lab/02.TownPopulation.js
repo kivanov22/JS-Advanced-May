cb0d4e9ca23285be605ce28103da2025b1fb05b2
Kristian Ivanov <kriskopk@abv.bg>
2021-05-31 15:58:52 +0300

    Updated Town Population

--- Objects-And-Composition/Lab/02.TownPopulation.js
+++ Objects-And-Composition/Lab/02.TownPopulation.js
@@ -1,5 +1,6 @@
 function solve(townsArr) {
     const towns = {};
+    
     for (let townAsString of townsArr) {
         let [name, population] = townAsString.split(' <-> ');
         population = Number(population);
@@ -20,3 +21,12 @@ console.log(
     "Las Vegas <-> 1000000",
   ])
 );
+// for (let townAsString of townsArr) {
+//         let [name, population] = townAsString.split(' <-> ');
+//         population = Number(population);
+//         if (towns[name] != undefined) { population += towns[name]; }
+//         towns[name] = population;
+//     }
+//     for (let town in towns) {
+//         console.log(`${town} : ${towns[town]}`);
+//     }
