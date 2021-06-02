f7b9e97c251db702cdcab0d0e2b4a5e7e7a04b07
Kristian Ivanov <kriskopk@abv.bg>
2021-06-02 18:35:16 +0300

    Calorie Object

---
+++ Objects-And-Composition/Exercise/01.CalorieObject.js
@@ -0,0 +1,10 @@
+function calories(arr){
+    const result = {};
+    
+    for (let i = 0; i < arr.length; i+=2) {
+        
+       result[arr[i]]=Number(arr[i+1]);
+    }
+    return result;
+}
+console.log(calories(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
