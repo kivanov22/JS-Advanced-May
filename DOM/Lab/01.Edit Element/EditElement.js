fad0462eeb06ddc47f71d0808819a5c00786253e
Kristian Ivanov <kriskopk@abv.bg>
2021-06-06 16:15:17 +0300

    Updated EditElement

--- DOM/Lab/01.Edit Element/EditElement.js
+++ DOM/Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(reff,match,replacer) {
+   const content =reff.textContent;
+   const matcher = new RegExp(match, 'g');
+   const edit =content.replace(matcher, replacer);
+   reff.textContent =edit;
 }
