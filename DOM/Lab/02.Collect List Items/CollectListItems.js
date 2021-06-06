f0c677e25869391e415354e99f52392539d91c63
Kristian Ivanov <kriskopk@abv.bg>
2021-06-06 16:34:03 +0300

    Updated Collect List Items

--- DOM/Lab/02.Collect List Items/CollectListItems.js
+++ DOM/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+   let listItems =document.querySelectorAll('#items li');
+   let result ='';
+
+   for (const listItem of listItems) {
+       result+=listItem.textContent.trim() + '\n';
+   }
+   let resultElement = document.getElementById('result');
+   resultElement.textContent= result.trim();
 }
