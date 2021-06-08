1884dfeebb474793d820460d4878e8d6850e7446
Kristian Ivanov <kriskopk@abv.bg>
2021-06-08 20:21:33 +0300

    Updated

--- DOM-Manipulations-And-Events/Lab/02. Add-Delete/app.js
+++ DOM-Manipulations-And-Events/Lab/02. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+  //take listItems and newItem which we gonna write
+  let listItems = document.getElementById("items");
+  let newItem = document.getElementById("newItemText");
+
+  //create new li element
+  let liElement = document.createElement("li");
+  liElement.textContent = newItem.value;
+
+  //Add Delete button
+  let removeElement = document.createElement("a");
+  // let linkText = document.createTextNode('[Delete]');
+  removeElement.setAttribute("href", "#");
+  removeElement.textContent = "[Delete]";
+
+  //Event listener for deleting element
+  removeElement.addEventListener("click", (e) => {
+    e.currentTarget.parentNode.remove();
+  });
+
+  liElement.appendChild(removeElement);
+  listItems.appendChild(liElement);
+  
+  //Emptying the writing field
+  newItem.value = "";
 }
