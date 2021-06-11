68cbd82f77f9b8b1d44d95bb28e21db9b0e61de6
Kristian Ivanov <kriskopk@abv.bg>
2021-06-11 17:03:52 +0300

    Updated

--- DOM/Exercise/05. Table - Search Engine/solution.js
+++ DOM/Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,25 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-console.log('Kris');
+     let searchFieldElement = document.getElementById('searchField');
+     let searchValue = searchFieldElement.value;
+     let listElements = Array.from(document.querySelectorAll('tbody tr'));
+
+     //clear styles from previous search
+     listElements.forEach(el=>{
+        el.className='';
+     });
+
+     //find matching row elements
+     //modify style for matching rows
+
+     let filteredRows = listElements.filter(el=>{
+        let values = Array.from(el.children);//tr take td tags
+        if (values.some(x=>x.textContent.includes(searchValue))) {
+           el.className='select';
+        }
+     });
+    
+    searchFieldElement.value='';
    }
 }
