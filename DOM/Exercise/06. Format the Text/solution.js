880c942503bd87749279e645b41fc4cf9646015e
Kristian Ivanov <kriskopk@abv.bg>
2021-06-11 17:04:18 +0300

    Updated

--- DOM/Exercise/06. Format the Text/solution.js
+++ DOM/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
-}
+ let textArea = document.getElementById('input');
+
+ let text = textArea.value;
+//split text by '.' to get sentences
+ let sentences = text.split('.').filter(x=>x !=='').map(x=> x + '.');
+
+ //group sentences by 3 in paragraphs 
+
+ let paragraphRoof = Math.ceil(sentences.length/3);
+
+ //insert paragraphs into DOM
+
+ let resultDiv = document.getElementById('output');
+  
+ for (let i = 0; i < paragraphRoof; i++) {
+   resultDiv.innerHTML+=(`<p>${sentences.splice(0,3).join('')}</p>`);
+ 
+  }
+ }
+
